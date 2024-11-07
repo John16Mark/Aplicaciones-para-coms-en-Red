97d7afc91113e3da1c0535075edd5eae1a6780e4
John16Mark <jlma-113@hotmail.com>
2024-11-07 00:50:00 -0600

    Recbir esperado

--- Práctica2/src/Cliente.java
+++ Práctica2/src/Cliente.java
@@ -14,6 +14,8 @@ public class Cliente {
     final public static int TAM_BUFFER = 65535;
     final static String dir_host = "127.0.0.1";
     final static int PORT = 5555;
+    final static int TIEMPO_ESPERA = 2000;
+
     final static String ruta = "./archivo.txt";
 
     public static void main(String[] args){
@@ -23,12 +25,10 @@ public class Cliente {
 
             int tam = TAM_VENTANA;
             int MAX_PAQUETES = (int) bytes.length/TAM_VENTANA;
-            /*if((int) tmp.length % TAM_VENTANA != 0)
-                MAX_PAQUETES++;*/
+
             ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
             DataOutputStream outStream = new DataOutputStream(byteOut);
             DatagramSocket socket = new DatagramSocket();
-
             InetAddress direccion = InetAddress.getByName(dir_host);
 
             for(int i=0; i<MAX_PAQUETES; i++){
@@ -49,7 +49,7 @@ public class Cliente {
 
             if((int) bytes.length % TAM_VENTANA != 0) {
                 int n_sobrantes = (int) bytes.length % TAM_VENTANA;
-                outStream.writeInt(MAX_PAQUETES); // Número de paquete
+                outStream.writeInt(MAX_PAQUETES);   // Número de paquete
                 byte[] btmp = Arrays.copyOfRange(bytes, (MAX_PAQUETES)*tam, (MAX_PAQUETES)*tam + n_sobrantes);
                 outStream.writeInt(btmp.length);    // Tamaño de paquete
                 outStream.write(btmp);              // Datos
--- Práctica2/src/Servidor.java
+++ Práctica2/src/Servidor.java
@@ -1,7 +1,12 @@
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Servidor {
 
@@ -9,12 +14,19 @@ public class Servidor {
     final public static int TAM_BUFFER = 65535;
     final static int PORT = 5555;
 
+    final static String dir_server = "./server/";
+    final static String nombre_archivo = "archivo.txt";
+
     public static void main(String[] args) {
-        try {
-            // Crear el socket
-            DatagramSocket socket = new DatagramSocket(PORT);
+        // Crear el socket para recibir
+        try (DatagramSocket socket = new DatagramSocket(PORT)) {
             socket.setReuseAddress(true);
             System.out.println("\033[94mServidor abierto\nEsperando datagrama...\033[0m");
+            int expectedPacket = 0;
+
+            // Clases para enviar información
+            ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+            DataOutputStream outStream = new DataOutputStream(byteOut);
 
             while(true) {
                 // Recibir paquete
@@ -29,8 +41,23 @@ public class Servidor {
                 byte[] b = new byte[tam];           // datos en bytes
                 int x = inStream.read(b);
                 String cadena = new String(b);      // Cadena de los bytes
-                System.out.println("\033[92mPaquete recibido. \033[95m#paquete: \033[0m"+ n+ "\t\033[95mtam: \033[0m"+tam+" bytes\tx: \033[0m"+x+"\nmensaje:"+cadena);
+                
+                if(expectedPacket == n) {
+                    outStream.writeInt(n);
+                    outStream.flush();
+                    byte[] bufferOut = byteOut.toByteArray();
+                    DatagramPacket ACK = new DatagramPacket(bufferOut, bufferOut.length, packet.getAddress(), packet.getPort());
+                    socket.send(ACK);
+                    byteOut.reset();
+                    expectedPacket++;
+                }
+                
+                System.out.println("\033[92mPaquete recibido. \033[95m#paquete: \033[0m"+ n+ "\t\033[95mtam: \033[0m"+tam+" bytes\t\033[95mx: \033[0m"+x);
+                System.out.println(cadena);
                 inStream.close();
+
+                Path path = Paths.get(dir_server+nombre_archivo);
+                Files.write(path, b);
             }
         }
         catch(Exception e) {
